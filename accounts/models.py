bd679eaa7c2e42c9e0b3d4f43ac8abaeab6bf571
secona <77039267+secona@users.noreply.github.com>
2024-10-18 22:06:46 +0700

    feat: remove proxy models to simplify

--- accounts/models.py
+++ accounts/models.py
@@ -1,34 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class User(AbstractUser):
     class Role(models.TextChoices):
         FOODIE = 'Foodie'
         MERCHANT = 'Merchant'
 
-    base_role: Role
+    role = models.CharField(max_length=50, choices=Role.choices, default=Role.FOODIE)
 
-    role = models.CharField(max_length=50, choices=Role.choices)
-
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.role = self.base_role
-        return super().save(*args, **kwargs)
-
-class Foodie(User):
-    base_role = User.Role.FOODIE
-
-    class Meta:
-        proxy = True
+@receiver(post_save, sender=User)
+def autocreate_profile(sender, instance: User, created, **kwargs):
+    if created:
+        match instance.role:
+            case User.Role.FOODIE:
+                FoodieProfile.objects.create(user=instance)
+            case User.Role.MERCHANT:
+                MerchantProfile.objects.create(user=instance)
 
 class FoodieProfile(models.Model):
-    foodie = models.OneToOneField(Foodie, on_delete=models.CASCADE)
-
-class Merchant(User):
-    base_role = User.Role.MERCHANT
-
-    class Meta:
-        proxy = True
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class MerchantProfile(models.Model):
-    merchant = models.OneToOneField(Merchant, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
