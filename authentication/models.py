c9af967f20b8d5a0cfdc9083e635f49aa6657605
secona <77039267+secona@users.noreply.github.com>
2024-10-18 21:33:10 +0700

    feat: User model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        FOODIE = 'Foodie'
+        MERCHANT = 'Merchant'
+
+    base_role: Role
+
+    role = models.CharField(max_length=50, choices=Role.choices)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+        return super().save(*args, **kwargs)
+
+class Foodie(User):
+    base_role = User.Role.FOODIE
+
+    class Meta:
+        proxy = True
+
+class FoodieProfile(models.Model):
+    foodie = models.OneToOneField(Foodie, on_delete=models.CASCADE)
+
+class Merchant(User):
+    base_role = User.Role.MERCHANT
+
+    class Meta:
+        proxy = True
+
+class MerchantProfile(models.Model):
+    merchant = models.OneToOneField(Merchant, on_delete=models.CASCADE)
--- paku/settings.py
+++ paku/settings.py
@@ -128,3 +128,5 @@ else:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
