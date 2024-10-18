5c38026204cea8680e239a81fabfe7cab0fb97a6
secona <77039267+secona@users.noreply.github.com>
2024-10-18 23:27:28 +0700

    feat: implement login register

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from .models import User
+from .forms import UserRegistrationForm
 
 def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('accounts:home')
+        else:
+            messages.error(request, "Username or Password does not match")
+
     return render(request, 'login.html')
 
 def register_page(request):
-    return render(request, 'register.html')
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been successfully created!")
+            return redirect('accounts:login')
+    
+    form = UserRegistrationForm()
+    return render(request, 'register.html', {'form':form})
+
+def home_page(request):
+    return render(request, 'home.html', {'user': request.user})
