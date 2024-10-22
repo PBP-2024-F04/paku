4c4fc9b9692ec3a5214ec4aaa70bbc35a73028d8
secona <77039267+secona@users.noreply.github.com>
2024-10-23 00:45:35 +0700

    feat: register form role

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,18 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from accounts.models import User
+from accounts.models import FoodieProfile, MerchantProfile, User
 
 class UserRegistrationForm(UserCreationForm):
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username',)
+        fields = ['username', 'password1', 'password2', 'role']
+
+class FoodieProfileForm(forms.ModelForm):
+    class Meta:
+        model = FoodieProfile
+        fields = ['full_name']
+
+class MerchantProfileForm(forms.ModelForm):
+    class Meta:
+        model = MerchantProfile
+        fields = ['restaurant_name']
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
-from .models import User
-from .forms import UserRegistrationForm
+from .models import FoodieProfile, User
+from .forms import FoodieProfileForm, MerchantProfileForm, UserRegistrationForm
 
 def login_page(request):
     if request.method == "POST":
@@ -23,12 +23,36 @@ def register_page(request):
         form = UserRegistrationForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            user = form.save()
+
+            if user.role == 'Foodie':
+                foodie_form = FoodieProfileForm(request.POST)
+
+                if foodie_form.is_valid():
+                    foodie: FoodieProfile = foodie_form.save(commit=False)
+                    foodie.user = user
+                    foodie.save()
+                
+            if user.role == 'Merchant':
+                merchant_form = MerchantProfileForm(request.POST)
+
+                if merchant_form.is_valid():
+                    merchant = merchant_form.save(commit=False)
+                    merchant.user = user
+                    merchant.save()
+
             messages.success(request, "Your account has been successfully created!")
             return redirect('accounts:login')
     
     form = UserRegistrationForm()
-    return render(request, 'register.html', {'form':form})
+    foodie_form = FoodieProfileForm()
+    merchant_form = MerchantProfileForm()
+
+    return render(request, 'register.html', {
+        'form': form,
+        'foodie_form': foodie_form,
+        'merchant_form': merchant_form,
+    })
 
 def home_page(request):
     return render(request, 'home.html', {'user': request.user})
