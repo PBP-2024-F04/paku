e6ad34dad95d56752ce0288a855b54c757cc36ee
rizqyazzahra <rizqyazzahraa@gmail.com>
2024-10-23 16:23:51 +0700

    model product

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import main
+from products.views import main, create_product
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', main, name='main')
+    path('', main, name='main'),
+    path('create-product', create_product, name='create_product'),
 ]
