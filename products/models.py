31a43c990f4a4dae703fc90f98edd649ecb3af05
rizqyazzahra <rizqyazzahraa@gmail.com>
2024-10-23 17:00:30 +0700

    update model product

--- products/models.py
+++ products/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     product_name = models.CharField(max_length=255)
     restaurant = models.CharField(max_length=255)
     price = models.IntegerField()
