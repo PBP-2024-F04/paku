cb1b31d8487009648d532d82e784a9d0b8c322c6
secona <77039267+secona@users.noreply.github.com>
2024-10-23 20:58:25 +0700

    fix: OneToOneField for Post model

--- timeline/models.py
+++ timeline/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from accounts.models import User
 
 class Post(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
