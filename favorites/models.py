285f6743fda6f1bc0d45573e1eefa5165b39c8fc
nelsha <shakira.putrindari@gmail.com>
2024-10-23 17:15:40 +0700

    edit: model add ID

--- favorites/models.py
+++ favorites/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from products.models import Product
 from accounts.models import User
@@ -8,6 +9,7 @@ class Favorite(models.Model):
         CANT_GET_ENOUGH = 'loving_it', 'Loving It'
         ALL_TIME_FAVORITES = 'all_time_favorites', 'All Time Favorites'
 
+    favorite_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     foodie = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role': 'Foodie'})
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     category = models.CharField(max_length=20, choices=Category.choices)
