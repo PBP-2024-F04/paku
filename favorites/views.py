c8303d7fcdc90bb4f8602d76173384557e471900
nelsha <shakira.putrindari@gmail.com>
2024-10-24 14:37:13 +0700

    fix user catch and add profile fav

--- favorites/views.py
+++ favorites/views.py
@@ -3,13 +3,14 @@ from django.http import HttpResponseRedirect
 from products.models import Product
 from .models import Favorite
 from .forms import FavoriteForm
+from accounts.models import User, FoodieProfile
 
 def main(request):
     context = {
         'user': request.user,
-        'want_to_try': Favorite.objects.filter(user=request.user, category='want_to_try'),
-        'loving_it': Favorite.objects.filter(user=request.user, category='loving_it'),
-        'all_time_favorites': Favorite.objects.filter(user=request.user, category='all_time_favorites')
+        'want_to_try': Favorite.objects.filter(foodie=request.user, category='want_to_try'),
+        'loving_it': Favorite.objects.filter(foodie=request.user, category='loving_it'),
+        'all_time_favorites': Favorite.objects.filter(foodie=request.user, category='all_time_favorites')
     }
 
     return render(request, 'favorites.html', context)
@@ -44,3 +45,20 @@ def delete_favorite(request, product_id):
     product = Product.objects.get(pk = product_id) 
     product.delete()
     return HttpResponseRedirect(reverse('favorites:main'))
+
+def user_favorites(request, user_id):
+    profile_user = User.objects.get(pk = user_id)
+    favorites = Favorite.objects.filter(foodie=profile_user)
+
+    # Filter berdasarkan kategori
+    want_to_try = favorites.filter(category='craving')
+    loving_it = favorites.filter(category='cant_get_enough')
+    all_time_favorites = favorites.filter(category='all_time_favorites')
+
+    context = {
+        'profile_user': profile_user,
+        'want_to_try': want_to_try,
+        'loving_it': loving_it,
+        'all_time_favorites': all_time_favorites,
+    }
+    return render(request, 'favorites/profile_favorites.html', context)
