1f60dc8e69f0b1a9c7dbfd6e9be9b1b58c816e81
naphiale <nafia.levana19@gmail.com>
2024-10-24 19:09:16 +0700

    add id

--- reviews/models.py
+++ reviews/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from accounts.models import User
 from products.models import Product
+import uuid
 
 # Create your models here.
 class Review(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     rating = models.IntegerField()
