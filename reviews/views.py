cdf542fada9095546ea8dd852ada3f782f47a0e8
naphiale <nafia.levana19@gmail.com>
2024-10-24 12:51:14 +0700

    fix navbar links to point to correct urls

--- reviews/views.py
+++ reviews/views.py
@@ -6,7 +6,7 @@ from products.models import Product
 from .forms import ReviewForm
 
 # Show all reviews + My reviews
-def reviews_page(request):
+def main(request):
     all_reviews = Review.objects.all()
     
     if request.user.is_authenticated:
@@ -14,18 +14,13 @@ def reviews_page(request):
     else:
         my_reviews = None
     
-    context = {
-        'all_reviews': all_reviews,
-        'my_reviews': my_reviews,
-    }
-    
-    return render(request, 'reviews/reviews.html', context)
+    return render(request, 'reviews.html', {'all_reviews': all_reviews, 'my_reviews': my_reviews})
 
 # Show reviews for a specific product
 def product_reviews(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     reviews = Review.objects.filter(product=product)
-    return render(request, 'reviews/product_reviews.html', {'product': product, 'reviews': reviews})
+    return render(request, 'product_reviews.html', {'product': product, 'reviews': reviews})
 
 # Create a new review
 @login_required
@@ -41,7 +36,7 @@ def create_review(request, product_id):
             return redirect('reviews:product_reviews', product_id=product.id)
     else:
         form = ReviewForm()
-    return render(request, 'reviews/create_review.html', {'form': form, 'product': product})
+    return render(request, 'create_review.html', {'form': form, 'product': product})
 
 # Edit an existing review
 @login_required
@@ -49,17 +44,17 @@ def edit_review(request, review_id):
     review = get_object_or_404(Review, id=review_id)
     
     if request.user != review.user:
-        return redirect('reviews:show_review')  # Prevent editing if not the owner
+        return redirect('reviews:main')  # Prevent editing if not the owner
     
     if request.method == 'POST':
         form = ReviewForm(request.POST, instance=review)
         if form.is_valid():
             form.save()
-            return redirect('reviews:show_review')
+            return redirect('reviews:product_reviews', product_id=review.product.id)  # Redirect to the product's reviews
     else:
         form = ReviewForm(instance=review)
     
-    return render(request, 'reviews/create_review.html', {'form': form, 'product': review.product})
+    return render(request, 'create_review.html', {'form': form, 'product': review.product})
 
 # Delete a review
 @login_required
@@ -69,4 +64,4 @@ def delete_review(request, review_id):
     if request.user == review.user:
         review.delete()
     
-    return HttpResponseRedirect(reverse('reviews:show_review'))
+    return HttpResponseRedirect(reverse('reviews:main'))
