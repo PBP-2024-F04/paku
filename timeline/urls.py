45dd3ac55c83490a975268ea7711a9a2e584b961
secona <77039267+secona@users.noreply.github.com>
2024-10-24 10:22:27 +0700

    feat: change timeline post urls

--- timeline/urls.py
+++ timeline/urls.py
@@ -5,11 +5,11 @@ app_name = 'timeline'
 
 urlpatterns = [
     path('', main, name='main'),
-    path('create/', create_post, name='create_post'),
-    path('view/<uuid:post_id>', view_post, name='view_post'),
-    path('edit/<uuid:post_id>', edit_post, name='edit_post'),
-    path('delete/<uuid:post_id>', delete_post, name='delete_post'),
+    path('post/', create_post, name='create_post'),
+    path('post/<uuid:post_id>', view_post, name='view_post'),
+    path('post/<uuid:post_id>/edit', edit_post, name='edit_post'),
+    path('post/<uuid:post_id>/delete', delete_post, name='delete_post'),
 
-    path('<uuid:post_id>/comment', get_comments, name='get_comments'),
-    path('<uuid:post_id>/comment/create', create_comment, name='create_comment'),
+    path('post/<uuid:post_id>/comment', get_comments, name='get_comments'),
+    path('post/<uuid:post_id>/comment/create', create_comment, name='create_comment'),
 ]
