40e41ef62a0d54906e599c580d0c2b18e8996c32
naphiale <nafia.levana19@gmail.com>
2024-10-24 12:06:56 +0700

    add models, urls, views, forms, templates

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from accounts.models import User
+from products.models import Product
 
 # Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import Review
+from products.models import Product
+from .forms import ReviewForm
 
-def main(request):
-    return render(request, 'reviews.html')
+# Show all reviews + My reviews
+def show_review(request):
+    all_reviews = Review.objects.all()
+    
+    if request.user.is_authenticated:
+        my_reviews = Review.objects.filter(user=request.user)
+    else:
+        my_reviews = None
+    
+    context = {
+        'all_reviews': all_reviews,
+        'my_reviews': my_reviews,
+    }
+    
+    return render(request, 'reviews/reviews.html', context)
+
+# Show reviews for a specific product
+def product_reviews(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    reviews = Review.objects.filter(product=product)
+    return render(request, 'reviews/product_reviews.html', {'product': product, 'reviews': reviews})
+
+# Create a new review
+@login_required
+def create_review(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product
+            review.user = request.user
+            review.save()
+            return redirect('reviews:product_reviews', product_id=product.id)
+    else:
+        form = ReviewForm()
+    return render(request, 'reviews/create_review.html', {'form': form, 'product': product})
+
+# Edit an existing review
+@login_required
+def edit_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    
+    if request.user != review.user:
+        return redirect('reviews:show_review')  # Prevent editing if not the owner
+    
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('reviews:show_review')
+    else:
+        form = ReviewForm(instance=review)
+    
+    return render(request, 'reviews/create_review.html', {'form': form, 'product': review.product})
+
+# Delete a review
+@login_required
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    
+    if request.user == review.user:
+        review.delete()
+    
+    return HttpResponseRedirect(reverse('reviews:show_review'))
