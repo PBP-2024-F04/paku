bdb36e6694f053e8b361d9ccf2df35ef1470e913
naphiale <nafia.levana19@gmail.com>
2024-10-24 12:29:51 +0700

    edit urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,9 +4,9 @@ from .views import *
 app_name = 'reviews'
 
 urlpatterns = [
-    path('', show_review, name='show_review'),
+    path('reviews', show_review, name='show_review'),
     path('product/<int:product_id>/reviews/', product_reviews, name='product_reviews'),
     path('product/<int:product_id>/reviews/create/', create_review, name='create_review'),
-    path('my-reviews/<int:review_id>/edit/', edit_review, name='edit_review'),
-    path('my-reviews/<int:review_id>/delete/', delete_review, name='delete_review'),
+    path('reviews/me/<int:review_id>/edit/', edit_review, name='edit_review'),
+    path('reviews/me/<int:review_id>/delete/', delete_review, name='delete_review'),
 ]
