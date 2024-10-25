da5aa18ffe1b729a9d0efbd74889a14056bf8661
naphiale <nafia.levana19@gmail.com>
2024-10-25 13:49:09 +0700

    fix rating

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,7 +5,4 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['comment', 'rating']  # Assuming your review model has 'content' and 'rating'
-        widgets = {
-            'comment': forms.Textarea(attrs={'class': 'form-input', 'placeholder': 'Write your review...'}),
-            'rating': forms.Select(choices=[(i, str(i)) for i in range(1, 6)], attrs={'class': 'form-select'})
-        }
+
