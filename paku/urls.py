f0b02f783428c57da7fc092d06085efb145e6c67
secona <77039267+secona@users.noreply.github.com>
2024-10-27 17:20:02 +0700

    feat: add profiles app

--- paku/urls.py
+++ paku/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('favorites/', include('favorites.urls')),
     path('products/', include('products.urls')),
+    path('profile/', include('profiles.urls')),
     path('promos/', include('promos.urls')),
     path('reviews/', include('reviews.urls')),
     path('timeline/', include('timeline.urls')),
