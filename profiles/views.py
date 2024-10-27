a25a82a30a6bc9a6a224d19427c6b97a780b72be
secona <77039267+secona@users.noreply.github.com>
2024-10-27 17:34:20 +0700

    feat: simple profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from accounts.models import User
+
+def profile_page(request, username):
+    user = get_object_or_404(User, username=username)
+    return render(request, 'profile.html', {'user': user})
