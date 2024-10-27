b38a6e712ec5600accfc2393d209500ce88d0e14
secona <77039267+secona@users.noreply.github.com>
2024-10-27 18:03:04 +0700

    feat: simple foodie profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 
+app_name = 'profiles'
+
 urlpatterns = [
     path('@<str:username>', profile_page, name='profile_page')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 
 from accounts.models import User
+from timeline.models import Post
 
 def profile_page(request, username):
     user = get_object_or_404(User, username=username)
-    return render(request, 'profile.html', {'user': user})
+    posts = Post.objects.filter(user=user)
+    return render(request, 'profile.html', {'user': user, 'posts': posts})
