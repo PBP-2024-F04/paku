0cd974a982724a2f093fbc431dc1e4c023d99af5
nelsha <shakira.putrindari@gmail.com>
2024-12-02 16:04:42 +0700

    edit import in views

--- favorites/views.py
+++ favorites/views.py
@@ -7,8 +7,8 @@ from accounts.models import User
 from django.http import JsonResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from django.core.serializers import serialize
-from django.http import JsonResponse
+from django.core import serializers
+from django.http import HttpResponse
 
 @login_required(login_url='/accounts/login')
 def main(request):
