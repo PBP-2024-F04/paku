edc5939fdb323aad65a88aef9ffd41feec60147c
naphiale <nafia.levana19@gmail.com>
2024-12-21 11:15:03 +0700

    add endpoint

--- reviews/urls.py
+++ reviews/urls.py
@@ -9,4 +9,13 @@ urlpatterns = [
     path('product/<uuid:product_id>/reviews/create/', create_review, name='create_review'),
     path('reviews/me/<uuid:review_id>/edit/', edit_review, name='edit_review'),
     path('reviews/me/<uuid:review_id>/delete/', delete_review, name='delete_review'),
+
+    path('json/', show_json, name='show_json'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    path('json-reviews/', get_reviews_flutter, name='get_reviews_flutter'),
+    path('json-reviews-me/', get_my_reviews_flutter, name='get_my_reviews_flutter'),
+    path('json/product/<uuid:product_id>/reviews/', product_review_json, name='product_review_json'),
+    path('json/product/<uuid:product_id>/reviews/create/', create_review_json, name='create_review_json'),
+    path('json/reviews/me/<uuid:review_id>/edit/', edit_review_json, name='edit_review_json'),
+    path('json/reviews/me/<uuid:review_id>/delete/', delete_review_json, name='delete_review_json'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,13 @@
+import json
 from django.shortcuts import render, get_object_or_404, redirect, reverse
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.core import serializers
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 from .models import Review
-from products.models import Product
 from .forms import ReviewForm
+from products.models import Product
 
 # show main review
 def main(request):
@@ -30,6 +34,69 @@ def main(request):
         'my_reviews': my_reviews,
         'merchant_reviews': merchant_reviews,
     })
+
+def show_json(request):
+    data = Review.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = Review.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def get_reviews_flutter(request):
+    reviews = Review.objects.all().order_by('-created_at')
+    data = [
+        {
+            "id": review.id,
+            "user": {
+                "role": review.user.role,
+                "display_name":
+                    review.user.foodieprofile.full_name
+                    if review.user.role == "Foodie" else
+                    review.user.merchantprofile.restaurant_name,
+                "username": review.user.username,
+            },
+            "product": {
+                "product_name": review.product.product_name,
+                "restaurant": review.product.restaurant,
+                "price": review.product.price,
+            },
+            "rating": review.rating,
+            "comment": review.comment,
+            "created_at": review.created_at,
+            "updated_at": review.updated_at,
+        }
+        for review in reviews
+    ]
+    return JsonResponse(data, content_type="application/json", safe=False)
+
+@login_required(login_url='/accounts/login')
+def get_my_reviews_flutter(request):
+    my_reviews = Review.objects.filter(user=request.user).order_by('-created_at')
+    data = [
+        {
+            "id": review.id,
+            "user": {
+                "role": review.user.role,
+                "display_name":
+                    review.user.foodieprofile.full_name
+                    if review.user.role == "Foodie" else
+                    review.user.merchantprofile.restaurant_name,
+                "username": review.user.username,
+            },
+            "product": {
+                "product_name": review.product.product_name,
+                "restaurant": review.product.restaurant,
+                "price": review.product.price,
+            },
+            "rating": review.rating,
+            "comment": review.comment,
+            "created_at": review.created_at,
+            "updated_at": review.updated_at,
+        }
+        for review in my_reviews
+    ]
+    return JsonResponse(data, content_type="application/json", safe=False)
 
 # Show reviews for a specific product
 def product_review(request, product_id):
@@ -89,3 +156,94 @@ def delete_review(request, review_id):
         review.delete()
     
     return HttpResponseRedirect(reverse('reviews:main'))
+
+def product_review_json(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    reviews = Review.objects.filter(product=product)
+    data = [
+        {
+            "id": review.id,
+            "user": {
+                "role": review.user.role,
+                "display_name":
+                    review.user.foodieprofile.full_name
+                    if review.user.role == "Foodie" else
+                    review.user.merchantprofile.restaurant_name,
+                "username": review.user.username,
+            },
+            "product": {
+                "product_name": review.product.product_name,
+                "restaurant": review.product.restaurant,
+                "price": review.product.price,
+            },
+            "rating": review.rating,
+            "comment": review.comment,
+            "created_at": review.created_at,
+            "updated_at": review.updated_at,
+        }
+        for review in reviews
+    ]
+    return JsonResponse(data, content_type="application/json", safe=False)
+
+@csrf_exempt
+@require_POST
+@login_required(login_url='/accounts/login')
+def create_review_json(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    
+    data = json.loads(request.body)
+    review_form = ReviewForm(data)
+
+    if review_form.is_valid():
+        review = review_form.save(commit=False)
+        review.product = product
+        review.user = request.user
+        review.save()
+
+        return JsonResponse({
+            "success": True,
+            "message": "Reviewed!"
+        }, status=200)
+
+    return JsonResponse({
+        "success": False,
+        "errors": review_form.errors,
+    }, status=400)
+
+@csrf_exempt
+@require_POST
+@login_required(login_url='/accounts/login')
+def edit_review_json(request, review_id):
+    data = json.loads(request.body)
+
+    instance = get_object_or_404(Review, pk=review_id, user=request.user)
+
+    review_form = ReviewForm(data, instance=instance)
+
+    if review_form.is_valid():
+        review: Review = review_form.save(commit=False)
+        review.is_edited = True
+        review.save()
+
+        return JsonResponse({
+            "success": True,
+            "message": "Edited!",
+        }, status=200)
+
+    return JsonResponse({
+        "success": False,
+        "errors": review_form.errors,
+    }, status=400)
+
+@csrf_exempt
+@require_POST
+@login_required(login_url='/accounts/login')
+def delete_review_json(request, review_id):
+    instance = get_object_or_404(Review, pk=review_id, user=request.user)
+
+    instance.delete()
+
+    return JsonResponse({
+        "success": True,
+        "message": "Deleted!",
+    }, status=200)
